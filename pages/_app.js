d51b7cbc0b5ae5a2bcd500d30536b6a3eb69ef59
brain545 <brain545@yandex.ru>
2021-01-31 00:47:05 +0300

    Images optimization disabled

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import Image from "next/image";
 import "tailwindcss/tailwind.css";
+
+const Image = ({ path }) => (
+  <div
+    className="rounded-lg"
+    style={{
+      backgroundImage: `url(${path})`,
+      height: 540,
+      width: 300
+    }}
+  />
+);
 
 const Preview = () => (
   <div className="min-h-screen flex flex-col items-center justify-center bg-pink-100">
@@ -7,30 +17,9 @@ const Preview = () => (
       Beautiful pictures with quotes of great people
     </div>
     <div className="flex flex-row flex-wrap justify-center md:space-x-4">
-      <div>
-        <Image
-          className="rounded-lg"
-          src="/preview1.png"
-          width={300}
-          height={540}
-        />
-      </div>
-      <div>
-        <Image
-          className="rounded-lg"
-          src="/preview2.png"
-          width={300}
-          height={540}
-        />
-      </div>
-      <div>
-        <Image
-          className="rounded-lg"
-          src="/preview3.png"
-          width={300}
-          height={540}
-        />
-      </div>
+      <Image path="preview1.png" />
+      <Image path="preview2.png" />
+      <Image path="preview3.png" />
     </div>
 
     <div className="max-w-xs">
